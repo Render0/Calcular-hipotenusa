76b3c557372c06b83c367bcb601920882c8837d2
Render0 <133005241+Render0@users.noreply.github.com>
2023-12-06 18:07:52 +0100

    Add files via upload

---
+++ JavaApplication6.java
@@ -0,0 +1,23 @@
+package javaapplication6;
+/**
+ *
+ * @author iagof
+ */
+public class JavaApplication6 {
+
+    public static void main(String[] args) {
+       
+        double  cateto1=60;
+        double cateto2= 80;
+        
+        double hypotenusa = Math.sqrt(Math.pow(cateto1, 2) + Math.pow(cateto2, 2));
+        
+        System.out.println("El resultado de la hipotenusa es " + hypotenusa);
+        
+        
+        
+        
+        
+    }
+
+}
